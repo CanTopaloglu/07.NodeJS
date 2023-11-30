b4a9f0b7dc0f0b6c8d78dad244635a34f0d01a1e
CanTopaloglu <emrecantopaloglu@gmail.com>
2023-11-30 23:48:32 +0300

    Initialize Project

---
+++ first.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+//command line Interface => CLI
+//Node.js => node / npm
+//Angular => ng
+//Next / React => node
+
+/*  
+Birden fazla satırda açıklama yazmamızı sağlar. 
+ */
+
+//node package manager => neredeyse tüm javascript kütüphaneleri burada depolanır.
+//npm (space) install example => JS kütüphanesini node_modules isimli klasöre indirir.
+
